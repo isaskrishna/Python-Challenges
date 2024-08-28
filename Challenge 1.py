0bf72861f3bc1333cd4c2e2186b3be980af49a60
isaskrishna <82940886+isaskrishna@users.noreply.github.com>
2024-08-28 09:35:00 +0100

    Create Challenge 1.py

---
+++ Challenge 1.py
@@ -0,0 +1,10 @@
+Name = input(“Hi there! What's your name?”)
+
+print(“Good to meet you”, name, “!”)
+
+
+Dish = input(“What is your favorite dish, ” + name + “ ?”)
+
+Country = input(“What is your favorite country, “ + name + “ ?”)
+
+print(name, “, I hope you enjoy a delicious portion of  “, Dish, “in “, Country, “ very soon!”)
